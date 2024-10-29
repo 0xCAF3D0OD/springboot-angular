dbe108ba11a8d1efd0e5737f5379c63636ce0f7a
unknown <tchappui@student.42lausanne.ch>
2024-10-29 21:59:19 +0100

    docker front + set up angular

---
+++ front/angular-app/src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  template: `
+    <h1>Welcome to {{title}}!</h1>
+    <h2>kevin</h2>
+
+    <router-outlet />
+  `,
+  styles: [],
+})
+export class AppComponent {
+  title = 'angular-app';
+}
